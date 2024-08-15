8ff51f513c54c79f7daa8bd719258a534b67ad00
shivam01 <shivamnamdev32@gmail.com>
2024-08-15 18:59:00 +0530

    add some code in Dad joke project

--- dadJoke/script.js
+++ dadJoke/script.js
@@ -1,25 +1,35 @@
 
 
 let button = document.getElementById('btn');
-let text=document.getElementById('joke');
+let text = document.getElementById('joke');
 
 async function joke() {
-       try {
-           let config = await fetch("https://icanhazdadjoke.com/", {
-               headers: {
-                   Accept: "application/json",
-               }
-           });
-   
-           if (!config.ok) {
-               throw new Error(`HTTP error! Status: ${config.status}`);
-           }
-   
-           let response = await config.json();
-           text.innerHTML=response.joke;
-       } catch (error) {
-           console.error("Failed to fetch a joke:", error.message);
-       }
-   }
-   
-   button.addEventListener('click',joke);
+       text.innerText = "Updating...";
+       button.innerText = "Loading...";
+        setTimeout(async function () {
+
+              try {
+              
+                     let config = await fetch("https://icanhazdadjoke.com/", {
+                            headers: {
+                                   Accept: "application/json",
+                            }
+                     });
+       
+                     if (!config.ok) {
+                            throw new Error(`HTTP error! Status: ${config.status}`);
+                     }
+       
+                     let response = await config.json();
+                     text.innerHTML = response.joke;
+                     button.innerText = "NEW JOKE";
+              } catch (error) {
+                     console.error("Failed to fetch a joke:", error.message);
+              }
+
+       },1000);
+       
+}
+
+
+button.addEventListener('click', joke);
